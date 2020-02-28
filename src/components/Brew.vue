64ae746a959bca5f1ecbb6e08a27a933c5c62224
manojkmishra <manojmishra571980@gmail.com>
2020-02-29 00:14:52 +1100

    bootstra cards with hover effect

--- src/components/Brew.vue
+++ src/components/Brew.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="hello">
-    <h1>Welcome to my Brew App!!</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center text-info">Brewery List</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6 brew-list">
+        <h2 class="text-center">left</h2>
+        <div class="card" style="width: 18rem;">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-6">
+        <h2 class="text-center">right</h2>
+      </div>
+    </div>
    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Brew',
   props: {
     msg: String
   }
@@ -14,19 +33,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+ .brew-list{
+   overflow-y:scroll;
+   height:95vh;
+   li{&:hover{background-color: darkgrey}}
+ }
+
 </style>
